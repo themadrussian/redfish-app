e7001307ed0266baa14b0ad351ddf2ef6a68f128
Mark Rogov <markrogov@yahoo.com>
2017-10-27 15:59:46 -0400

    got something

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-# index.js file
+// index.js file
+// require('babel-core/register');
+// require('babel-polyfill');
+// require('whatwg-fetch');
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+require('dotenv').config();
+
+var fetch = require('node-fetch');
+var https = require('https');
+const agent = new https.Agent({
+  rejectUnauthorized: false
+})
+
+// import 'whatwg-fetch';
+
+console.log("here");
+// export function fetch() {
+//   return () => {
+fetch('https://10.234.213.130/redfish/v1/', {
+// fetch('http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo', {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    // 'Auhtorization': 'Basic cm9vdDpwYXNzd29yZA==',
+  },
+}, { agent })
+.then((response) => response.json())
+.then((responseJson) => {
+  console.log("redfish:", responseJson);
+  //dispatch(Actions.jokeFetched(responseJson));
+})
+.catch((error) => {
+  console.error("fetch error:", error);
+});
+//   }
+// }
